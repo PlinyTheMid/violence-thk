38f06a5ae5d58b942dd7379230516a291ef9dbc4
Danny Joel Devarapalli <35692711+masterkenoobi5812@users.noreply.github.com>
2023-03-02 04:19:31 +0530

    Delete run-anpr.sh

--- YOLOX/tools/run-anpr.sh
+++
@@ -1,5 +0,0 @@
-image="graymatics1/anpr_yolox:v1"
-container="anpr_server"
-docker stop $container
-docker rm $container
-docker run --gpus all --name $container --network host --workdir /workspace/YOLOX/tools -it --entrypoint='python' $image yolox_s_server.py 
