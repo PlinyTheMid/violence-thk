653bcfe22e4f7afb97740e011c66758176b5a867
root <root@OfficeAm3.gbksoxzu404ezcaezej3tluo1c.ix.internal.cloudapp.net>
2023-03-01 21:13:35 +0000

    init-commit

---
+++ YOLOX/yolox/data/datasets/voc_classes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+# VOC_CLASSES = (
+#     "aeroplane",
+#     "bicycle",
+#     "bird",
+#     "boat",
+#     "bottle",
+#     "bus",
+#     "car",
+#     "cat",
+#     "chair",
+#     "cow",
+#     "diningtable",
+#     "dog",
+#     "horse",
+#     "motorbike",
+#     "person",
+#     "pottedplant",
+#     "sheep",
+#     "sofa",
+#     "train",
+#     "tvmonitor",
+# )
+
+VOC_CLASSES = (
+  "Registration_Plate",
+)
+
+# VOC_CLASSES = (
+#   "Honda",
+#   "Hyundai",
+#   "Mahindra",
+#   "Suzuki",
+#   "Toyota",
+#   "Volkswagen",
+#   "Chevrolet",
+#   "Force_Motors",
+#   "Tata",
+#   "Autorickshaw",
+#   "Skoda",
+# )
